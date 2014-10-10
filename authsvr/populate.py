795395cfb6513a025de7b8cb82f38f46a19c7972
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-10-10 13:22:41 -0700

    make admin user more real

--- authsvr/populate.py
+++ authsvr/populate.py
@@ -7,8 +7,8 @@ def populate_backend():
         INSERT INTO users (username, email_addr, desc, role, hash, creation_date) VALUES
         (
             'admin',
-            'admin@localhost.local',
-            'admin test user',
+            'admin@ccl.io',
+            '{"key1": "f64ef282-4410-47bb-9ebf-b8719d2a8a19", "key0": "4504dd6b-50ba-11e4-be6f-3c15c2e8aac8", "id": "45033d33-50ba-11e4-8e60-3c15c2e8aac8"}',
             'admin',
             'cLzRnzbEwehP6ZzTREh3A4MXJyNo+TV8Hs4//EEbPbiDoo+dmNg22f2RJC282aSwgyWv/O6s3h42qrA6iHx8yfw=',
             '2012-10-28 20:50:26.286723'
