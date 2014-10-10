d09ab83e8bb9b2f70969f813ff11b035b0119949
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-10-09 18:02:20 -0700

    add corky lab (external site)

---
+++ othersvr/index.js
@@ -0,0 +1,29 @@
+
+function out(msg) {
+    $('#out').html( $('#out').html() + "<li>" + msg + "</li>" );
+}
+
+
+
+
+function login() {
+    out("Start login...");
+    out("Oops.  Not yet implemented.  Sorry.");
+}
+
+function register() {
+    out("Start register...");
+    out("Oops.  Not yet implemented.  Sorry.");
+}
+
+function forgot() {
+    out("Start forgot...");
+    out("Oops.  Not yet implemented.  Sorry.");
+}
+
+
+function main() {
+    out("OK, Ready!");
+}
+
+$(main);
