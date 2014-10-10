a02930a7a29faf232b251a065d8423f5cf3a46b8
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-10-10 12:38:18 -0700

    make the install script actually work

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 virtualenv .v
 . env.sh
-pip install bottle flask bottle-cork
+pip install git+https://github.com/val314159/bottle-cork.git
+pip install flask
 pip install gevent
 
 (cd authsvr ; python populate.py)
